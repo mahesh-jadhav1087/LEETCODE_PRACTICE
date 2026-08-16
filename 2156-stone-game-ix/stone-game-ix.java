c298d263bf7096bda6a7daf3f8e204551e0ca145
Mahesh Jadhav <maheshjadhav1087@gmail.com>
2026-08-16 12:54:12 +0530

    Time: 4 ms (75.68%) | Memory: 114.6 MB (94.59%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] counts = new int[3];
+        for (int stone : stones) {
+            counts[stone % 3]++;
+        }
+        
+      
+        if (counts[0] % 2 == 0) {
+            
+            return counts[1] >= 1 && counts[2] >= 1;
+        }
+        
+        
+        return Math.abs(counts[1] - counts[2]) > 2;
+        
+    }
+}
